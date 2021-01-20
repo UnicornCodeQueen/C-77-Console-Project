57012250bfdde11f567d054be3e3df69a9a7ac41
UnicornCodeQueen <75204601+UnicornCodeQueen@users.noreply.github.com>
2021-01-20 19:40:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log("This is my first Console Project");
+console.log("My name is Sridevi");
+console.log("I don't want to say my full name as it is dangerous");
+console.log("15");
+console.log("5");
+console.log("50");
+console.log("2");
